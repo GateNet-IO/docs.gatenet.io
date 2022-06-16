7af0d7b664f43e5735a4ede8a3144aee568d9570
Sagar Upadhyay <usagar80@gmail.com>
2022-06-16 15:49:56 +0530

    New Version Files

--- docs/staking-user-guide/viewingYourTransactionHistory.md
+++
@@ -1,8 +0,0 @@
----
-sidebar_position: 6
----
-
-# Viewing Your Transaction History
-The bottom section of the page is the &quot;My Transaction History&quot; section. This section displays all the transactions linked to the connected wallet only. If no wallet is connected to the Staking Platform, the transaction history will be blank, the same goes for connecting a wallet to the Staking Platform for the first time where it has not performed any transactions:
-
-![view-transaction-list](/img/staking-user-guide/view-transaction-list.png)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
+const config = {
   title: 'GATENet Documentation',
   tagline: 'Documentation for GATENet Public Projects',
   url: 'https://docs.gatenet.io',
@@ -14,6 +14,14 @@ module.exports = {
   organizationName: 'GateNet-IO', // Usually your GitHub org/user name.
   projectName: 'docs.gatenet.io', // Usually your repo name.
   noIndex: process.env.NO_INDEX || false,
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
   plugins: [
     [
       'docusaurus2-dotenv',
@@ -25,18 +33,12 @@ module.exports = {
         expand: false, // Allows your variables to be "expanded" for reusability within your .env file
         defaults: false, //  Adds support for dotenv-defaults. If set to true, uses ./.env.defaults
       },
-    ],
-    [
-      require.resolve('docusaurus-lunr-search'),
-      {
-        indexBaseUrl: true,
-      },
-    ],
+    ]
   ],
 
   presets: [
     [
-      '@docusaurus/preset-classic',
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
@@ -53,12 +55,6 @@ module.exports = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      hideableSidebar: false,
-      colorMode: {
-        defaultMode: 'light',
-        disableSwitch: true,
-        respectPrefersColorScheme: true,
-      },
       navbar: {
         title: 'GATENet',
         logo: {
@@ -75,4 +71,6 @@ module.exports = {
         darkTheme: darkCodeTheme,
       },
     }),
-}
+};
+
+module.exports = config;
